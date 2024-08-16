874d94be6e878b59ba4363cfd667f578ac072912
himanshu-o5 <himanshushukla71.9@gmail.com>
2024-08-16 23:55:36 +0530

    NVChad + ME -> THIS

---
+++ lua/plugins/init.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    'goolord/alpha-nvim',
+    lazy = false,
+    config = function ()
+        require "configs.alpha"
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css"
+  		},
+  	},
+  },
+}
